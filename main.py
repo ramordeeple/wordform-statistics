ea169172b43d3971008970f07dbe56c46b34138f
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-23 18:37:52 +0300

    Main

---
+++ main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+from src.api.report import router
+from src.core.settings import settings
+
+app = FastAPI(title="Wordform statistics", debug=settings.DEBUG)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.APP_HOST,
+        port=settings.APP_PORT,
+        reload=settings.DEBUG
+    )
