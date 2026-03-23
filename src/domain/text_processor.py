b5f8ef09e74876aaef8fcdff9fc1a0fb58b07aff
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-23 18:39:19 +0300

    Fix crucial get_lemma

--- src/domain/text_processor.py
+++ src/domain/text_processor.py
@@ -1,3 +1,5 @@
+import re
+
 import pymorphy3
 
 from src.domain.constants import WORD_CLEANUP_PATTERN
@@ -5,11 +7,11 @@ from src.domain.constants import WORD_CLEANUP_PATTERN
 morph = pymorphy3.MorphAnalyzer()
 
 def get_lemma(word: str) -> str:
-    clean_word = WORD_CLEANUP_PATTERN.sub('', word.lower())
+    clean_word = re.sub(WORD_CLEANUP_PATTERN, "", word).lower()
 
-    if not clean_word or clean_word.isdigit():
+    if not clean_word:
         return ""
 
-    parsed = morph.parse(clean_word)
+    parsed = morph.parse(clean_word)[0]
 
-    return parsed[0].normal_form
+    return parsed.normal_form
