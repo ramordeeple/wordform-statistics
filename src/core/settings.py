df4c4a61481eb4d1dcccedc2b074348764779aee
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-23 13:41:24 +0300

    Add settings.py

---
+++ src/core/settings.py
@@ -0,0 +1,24 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from src.domain.constants import TEXT_ENCODING
+
+
+class Settings(BaseSettings):
+    FILE_REPORT_DIR: str = Field(default="./data/reports")
+
+    # 2 ГБ
+    MAX_UPLOAD_SIZE: int = Field(default=2 * 1024 * 1024 * 1024)
+    LOG_LEVEL: str = Field(default="INFO")
+    DEBUG: bool = Field(default=True)
+
+    APP_HOST: str = "127.0.0.1"
+    APP_PORT: int = 8000
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding=TEXT_ENCODING,
+        extra="ignore"
+    )
+
+settings = Settings()
