f383e7251ca55965880fb9cd28e2a545832410c2
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-23 13:33:58 +0300

    Add tech and business constants

---
+++ src/core/constants.py
@@ -0,0 +1,5 @@
+# Технические константы
+from typing import Final
+
+REPORT_EXTENSION: Final[str] = ".xlsx"
+REPORT_MIME_TYPE: Final[str] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
---
+++ src/domain/constants.py
@@ -0,0 +1,12 @@
+# Бизнес константы
+
+import re
+
+from typing import Final
+
+WORD_CLEANUP_PATTERN: Final[re.Pattern] = re.compile(r'[^\w]')
+TEXT_ENCODING: Final[str] = "utf-8"
+
+COLUMN_WORD: Final[str] = "Словоформа"
+COLUMN_TOTAL: Final[str] = "Всего во всем документе"
+COLUMN_PER_LINE: Final[str] = "Кол-во в каждой из строк"
